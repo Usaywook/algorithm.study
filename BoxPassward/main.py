7eb0f42b367bf2aa6255d6f58063e543d3dd2111
usaywook <sewookyoo@gmail.com>
2025-02-21 01:02:57 +0900

    passed

--- BoxPassward/main.py
+++ BoxPassward/main.py
@@ -1,21 +1,24 @@
+from memory import memory_usage_decorator 
 import sys
 sys.stdin = open("input.txt", "r")
 
-T = int(input())
-for t in range(T):
-    N, K = map(int, input().split())
+@memory_usage_decorator
+def solve():
+    T = int(input())
+    for t in range(T):
+        N, K = map(int, input().split())
 
-    M = N // 4
-    numbers = input()
-    candidates = set()
-    num = ''
-    for i in range(M):
-        for j in range(N):
-            k = -i + j 
-            x = numbers[k]
-            num += x 
-            if (i * N + j) % M == M - 1:
-                candidates.add(num)
-                num = ''        
-    print(f"#{t+1}", end=" ")
-    print(int(sorted(candidates, reverse=True)[K-1], 16))
+        M = N // 4
+        numbers = input()
+        candidates = set()        
+        for i in range(M): # M번 회전
+            for j in range(4): # 네 변의 숫자 추출
+                start_idx = j * M                
+                num = numbers[start_idx:start_idx + M]  # 슬라이싱 사용 (메모리 효율적)                
+                candidates.add(int(num, 16))
+            numbers = numbers[-1] + numbers[:-1]
+                                                                 
+        print(f"#{t+1}", end=" ")        
+        print(sorted(candidates, reverse=True)[K-1])
+        
+solve()
