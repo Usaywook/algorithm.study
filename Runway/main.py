b05ac6bf7c43fca9519f885ca92b3d07d1d2a34a
usaywook <sewookyoo@gmail.com>
2025-03-05 07:17:26 +0900

    edge case solved

--- Runway/main.py
+++ Runway/main.py
@@ -11,32 +11,31 @@ for test_case in range(1, T + 1):
     for i in range(N):        
         is_runway = True
         is_down = False
-        is_ramp = False
-        p = 0
-        q = 0        
+        p = 0   
         for j in range(1, N):
             prev, cur = array[i][j-1], array[i][j]
             diff = cur - prev
             if diff == 0: # 평지
-                if is_down and j - q + 1 >= X:
+                if is_down and j - p + 1 >= X:
                     is_runway = True
                     is_down = False
-                    p = j        
+                    p = j + 1  
             elif diff == 1: # 오르막
                 if is_down:
                     is_runway = False  
                     break
-                if j - p < X:
-                    is_runway = False
-                    break
                 else:
-                    p = j
+                    if j - p < X:
+                        is_runway = False
+                        break
+                    else:
+                        p = j
             elif diff == -1: # 내리막
                 if is_down:
                     is_runway = False  
                     break
                 else:
-                    q = j
+                    p = j
                     is_down = True
                     is_runway = False                    
             else:
@@ -49,36 +48,75 @@ for test_case in range(1, T + 1):
         is_runway = True
         is_down = False
         p = 0
-        q = 0
         for i in range(1, N):
             prev, cur = array[i-1][j], array[i][j]
             diff = cur - prev
             if diff == 0: # 평지
-                if is_down and i - q + 1 >= X:
+                if is_down and i - p + 1 >= X:
                     is_runway = True
                     is_down = False
-                    p = i        
+                    p = i + 1
             elif diff == 1: # 오르막
                 if is_down:
                     is_runway = False  
                     break
-                if i - p < X:
-                    is_runway = False
-                    break
                 else:
-                    p = i
+                    if i - p < X:
+                        is_runway = False
+                        break
+                    else:
+                        p = i
             elif diff == -1: # 내리막
                 if is_down:
                     is_runway = False  
                     break
                 else:
-                    q = i
+                    p = i
                     is_down = True
                     is_runway = False                    
             else:
                 is_runway = False
                 break
         cnt += int(is_runway)
-    
+        
     print(f'#{test_case} {cnt}')
-    
+
+
+# array = [3,2,2,2,2,2,2,2,3]
+# X = 4
+# N = len(array)
+
+# is_runway = True
+# is_down = False
+# p = 0
+# for j in range(1, N):
+#     prev, cur = array[j-1], array[j]
+#     diff = cur - prev
+#     if diff == 0: # 평지
+#         if is_down and j - p + 1 >= X:
+#             is_runway = True
+#             is_down = False
+#             p = j + 1
+#     elif diff == 1: # 오르막
+#         if is_down:
+#             is_runway = False  
+#             break
+#         else:
+#             if j - p < X:
+#                 is_runway = False
+#                 break
+#             else:
+#                 p = j
+#     elif diff == -1: # 내리막
+#         if is_down:
+#             is_runway = False  
+#             break
+#         else:
+#             p = j
+#             is_down = True
+#             is_runway = False                    
+#     else:
+#         is_runway = False
+#         break
+#     print(cur, p, j, is_runway)
+# print(cur, p, j, is_runway)
