f8c112df8d0aef1e817cb9a06f130472e6f030fa
usaywook <sewookyoo@gmail.com>
2025-03-12 22:44:23 +0900

    ref code add

--- DessertCafe/main.py
+++ DessertCafe/main.py
@@ -1,54 +1,54 @@
 import sys
 sys.stdin = open("input.txt", "r")
+sys.path.append('../')
+from evaluator import memory_usage_decorator
 
-di, dj = (-1, 1, 1, -1), (1, 1, -1, -1)
+@memory_usage_decorator
+def main():
+    di, dj = (-1, 1, 1, -1), (1, 1, -1, -1)
 
-T = int(input())
-for t in range(1, T+1):
-    N = int(input())
-    grid = []
-    for _ in range(N):
-        grid.append(list(map(int, input().split())))
-    
-    def search(i, j, si, sj, visited, checked, d=0, direction=0, length=-1):
-                                 
-        for k in range(direction, max(direction + 1, 4)):
-            ni, nj = i + di[k], j + dj[k]
-            
-            if ni < 0 or ni > N-1 or nj < 0 or nj > N-1:
-                continue              
-            
-            if d > 1 and (ni == si and nj == sj):                
-                length = max(length, d + 1)  
-                # print(f"visit ({si}, {sj}), depth = {d + 1}, checked = {checked}")    
-                # for v in visited:
-                #     print(v)
-                # print()
-                return length
-            
-            ne = grid[ni][nj]
-            if ne in checked:
-                continue
-            
-            if not visited[ni][nj]:      
-                visited[ni][nj] = 1
-                checked.add(grid[ni][nj])          
-                tmp = search(ni, nj, si, sj, visited, checked, d + 1, k, length)
-                length = max(length, tmp)  
-                visited[ni][nj] = 0
-                checked.remove(ne)                
+    T = int(input())
+    for t in range(1, T+1):
+        N = int(input())
+        grid = []
+        for _ in range(N):
+            grid.append(list(map(int, input().split())))
         
-        return length
-
-    ans = -1
-    for i in range(N):
-        for j in range(N):
-            visited = [[0] * N for _ in range(N)]     
-            visited[i][j] = 1     
-            checked = set([grid[i][j]])                   
-            cnt = search(i, j, i, j, visited, checked)            
-            ans = max(ans, cnt)
+        def search(i, j, si, sj, visited, checked, d=0, direction=0, length=-1):
+                                    
+            for k in range(direction, max(direction + 1, 4)):
+                ni, nj = i + di[k], j + dj[k]
+                
+                if ni < 0 or ni > N-1 or nj < 0 or nj > N-1:
+                    continue              
+                
+                if d > 1 and (ni == si and nj == sj):                
+                    length = max(length, d + 1)  
+                    return length
+                
+                ne = grid[ni][nj]
+                if ne in checked:
+                    continue
+                
+                if not visited[ni][nj]:      
+                    visited[ni][nj] = 1
+                    checked.add(grid[ni][nj])          
+                    tmp = search(ni, nj, si, sj, visited, checked, d + 1, k, length)
+                    length = max(length, tmp)  
+                    visited[ni][nj] = 0
+                    checked.remove(ne)                
             
-    print(f"#{t} {ans}")
+            return length
 
-    
+        ans = -1
+        for i in range(N):
+            for j in range(N):
+                visited = [[0] * N for _ in range(N)]     
+                visited[i][j] = 1     
+                checked = set([grid[i][j]])                   
+                cnt = search(i, j, i, j, visited, checked)            
+                ans = max(ans, cnt)
+                
+        print(f"#{t} {ans}")
+
+main()
---
+++ DessertCafe/ref.py
@@ -0,0 +1,48 @@
+import sys
+sys.stdin = open("input.txt", "r")
+sys.path.append('../')
+from evaluator import memory_usage_decorator
+
+# 대각선
+# 왼쪽 아래 
+# 오른쪽 아래 
+# 오른쪽 위 
+# 왼쪽 위 
+dr = [1, 1, -1, -1]
+dc = [-1, 1, 1, -1]
+
+def find_path():
+    for total_length in range(N - 1, 1, -1):
+        for l1 in range(1, total_length):
+            l2 = total_length - l1
+            for sr in range(N - l1 - l2):
+                for sc in range(l1, N - l2):
+                    v = [0] * 101
+                    r, c = sr, sc
+                    for d in range(4):
+                        l = l2 if d % 2 else l1
+                        for _ in range(l):
+                            r += dr[d]
+                            c += dc[d]
+                            if not v[MAP[r][c]]:
+                                v[MAP[r][c]] = 1
+                            else:
+                                break
+                        else:
+                            continue
+                        break
+                    else:
+                        return total_length * 2
+    return -1
+
+@memory_usage_decorator
+def main():
+    global T, N, MAP
+    T = int(input())
+    for tc in range(1, T + 1):
+        N = int(input())
+        MAP = [list(map(int, input().split())) for _ in range(N)]
+        ans = find_path()
+        print(f"#{tc} {ans}")
+
+main()
