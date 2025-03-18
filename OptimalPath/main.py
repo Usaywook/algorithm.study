a17c251be169cc9c0ed3661ddb60b91f73fa7ea3
usaywook <sewookyoo@gmail.com>
2025-03-18 16:01:54 +0900

    optimal path solved with inplace

--- OptimalPath/main.py
+++ OptimalPath/main.py
@@ -23,15 +23,15 @@ for t in range(1, T + 1):
 
         if len(visited) > N:            
             ans = min(ans, dist + getDist(cur_node, nodes[1]))
-            return
-
-        for next_ind in range(2, N+2):
-            next_node = nodes[next_ind]
-            if next_ind not in visited:
-                search(next_ind, dist + getDist(cur_node, next_node), visited | {next_ind})
+        else:
+            for next_ind in range(2, N+2):
+                next_node = nodes[next_ind]
+                if next_ind not in visited:
+                    search(next_ind, dist + getDist(cur_node, next_node), visited | {next_ind})
+        visited.remove(cur_ind)
 
     ans = 200 * N + 1
-    search(0, visited={0})
+    search(0)
     print(f"#{t} {ans}")
 
 
