8dd672531bb39c4421230accc59fc30bc04c5a45
usaywook <sewookyoo@gmail.com>
2025-03-30 21:57:57 +0900

    17136 color paper optimized with pruning and memoized search

--- Samsung/17136_colorpaper/main.py
+++ Samsung/17136_colorpaper/main.py
@@ -21,33 +21,34 @@ for t in range(1, T+1):
     grid = []
     for _ in range(10):
         grid.append(list(map(int, input().split())))
+    ones = [(i, j) for i in range(10) for j in range(10) if grid[i][j] == 1]
 
     counter = {i+1: 5 for i in range(5)}
-    def search(used_cnt=0):
+    def search(depth=0, used_cnt=0):
         global ans
         if used_cnt >= ans:
             return
 
-        for i in range(10):
-            for j in range(10):
-                if grid[i][j] == 0:
-                    continue
-                for k in range(5, 0, -1):
-                    if counter[k] <= 0:
-                        continue
-                    if is_attach(i, j, k):
-                        attach(i, j, k, 0)
-                        counter[k] -= 1
-                        search(used_cnt + 1)
-                        attach(i, j, k, 1)
-                        counter[k] += 1
-                return
-        ans = min(ans, used_cnt)
-        return
+        if depth == len(ones):
+            ans = min(ans, used_cnt)
+            return
+
+        i, j = ones[depth]
+        if grid[i][j] == 0:
+            search(depth+1, used_cnt)
+            return
+
+        for k in range(5, 0, -1):
+            if counter[k] <= 0:
+                continue
+            if is_attach(i, j, k):
+                attach(i, j, k, 0)
+                counter[k] -= 1
+                search(depth + 1, used_cnt + 1)
+                attach(i, j, k, 1)
+                counter[k] += 1
 
     ans = 25
-    search(0)
+    search()
     ans = -1 if ans == 25 else ans
     print(f"#{t} {ans}")
-
-
